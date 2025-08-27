bf2386a6b016d6b561383c017afdd1cefabead9d
Prasang41 <harshitchaudhary568@gmail.com>
2025-08-27 19:00:05 +0530

    Working on Home Page

--- display/src/components/Header/Header.tsx
+++ display/src/components/Header/Header.tsx
@@ -6,8 +6,8 @@ import { FaLongArrowAltRight } from 'react-icons/fa'
 
 function Header() {
     return (
-        <nav className='flex w-[99vw] m-0 p-0 '>
-            <div className='flex w-[20%] h-[100vh] fixed overflow-x-hidden'>
+        <nav className='flex w-[100vw] h-30 bg-yellow-50 m-0 p-0'>
+            <div className='flex w-[18%] h-[100vh] fixed overflow-x-hidden'>
                 <Link to='' className='bg-coral w-[50%] flex justify-center pt-10' >
                     < IoMdBook color='#FF667D' className='bg-white h-15 border-1 rounded-full w-auto p-4'/>
                 </Link>
@@ -18,10 +18,10 @@ function Header() {
             <div className='flex justify-around ml-[20%] w-[80vw] fixed pt-10'>
                 <div className='flex items-center justify-center gap-5'>
                     < BsFillEmojiSmileFill color='#FF9776' className='h-15 w-auto' />
-                    <h1 className='text-3xl text-gray-500'>Display<sup className='text-xl'>TM</sup></h1>
+                    <h1 className='text-3xl text-gray-500'>Display<sup className='text-sm font-bold'>TM</sup></h1>
                 </div>
-                <div className=' flex items-center w-25'>
-                    <p className='text-[10px] text-gray-500'>A CARE ORIENTED DESIGN STUDIO</p>
+                <div className=' flex items-center w-35'>
+                    <p className='text-sm text-gray-500 font-monospace'>A CARE ORIENTED DESIGN STUDIO</p>
                 </div>
                 <div className='flex justify-center items-center gap-5 bg-peach rounded-full w-35'>
                     <button>Contact</button>
--- display/src/components/Home/Home.tsx
+++ display/src/components/Home/Home.tsx
@@ -2,7 +2,12 @@
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="bg-yellow-50 ml-[18%]">
+      <div className="flex flex-col justify-center pl-[10%] h-120">
+        <h1 className="text-9xl text-gray-600">Shaping <span className="text-peach text-6xl">the</span></h1>
+        <h1 className="text-9xl text-gray-600 flex items-center"><span className="text-peach text-6xl">🌸 </span> Care Sector</h1>
+      </div>
+    </div>
   )
 }
 
